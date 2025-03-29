5fac760bd1d93abb2aaab3071ed9b2113a7c8f7c
Edward Newgate <ayeshulhassan@gmail.com>
2025-03-29 14:34:46 +0500

    FireBaseAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/pages/login_page.dart';
+import 'package:instagram_clone/pages/sign_up.dart';
 import 'package:instagram_clone/responsive/phone_screen.dart';
 import 'package:instagram_clone/responsive/responsive_screen.dart';
 import 'package:instagram_clone/responsive/web_screen.dart';
 import 'package:instagram_clone/utili/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyCA6Dx4BQAW7XDUNJxVb4BMNLEF0vZCwxM",
+        authDomain: "instagram-6c24b.firebaseapp.com",
+        projectId: "instagram-6c24b",
+        storageBucket: "instagram-6c24b.firebasestorage.app",
+        messagingSenderId: "623479456400",
+        appId: "1:623479456400:web:bfc4e0a196c0bef7e40e15",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -19,10 +39,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: ResponsiveScreen(
-        phoneScreenLayout: PhoneScreen(),
-        webScreenLayout: WebScreen(),
-      ),
+      // home: ResponsiveScreen(
+      //   phoneScreenLayout: PhoneScreen(),
+      //   webScreenLayout: WebScreen(),
+      // ),
+      home: SignUp(),
     );
   }
 }
